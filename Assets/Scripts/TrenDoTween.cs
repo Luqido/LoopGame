8765c4be0ee1c9a52808dab88a4085687106abf5
Luqido <tahafatihbjk28@gmail.com>
2025-07-31 20:54:37 +0300

    TrenSpritedegisimi

--- Assets/Scripts/TrenDoTween.cs
+++ Assets/Scripts/TrenDoTween.cs
@@ -5,16 +5,28 @@ public class TrenDoTween : MonoBehaviour
 {
     Animator TrenAnim;
     SpriteRenderer trenSprite;
+    [SerializeField] Sprite sp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         TrenAnim = GetComponent<Animator>();
+        trenSprite = GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void changeSprite()
+    {
+        TrenAnim.SetBool("Soldan", true);
+        if(TrenAnim.GetBool("Soldan"))
+        {
+            trenSprite.sprite = sp;
+        }
+
         
     }
    
