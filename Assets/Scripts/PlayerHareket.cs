0436e7139efd21380f59fdb9e2374a4d372ddd6e
Luqido <tahafatihbjk28@gmail.com>
2025-07-31 03:25:25 +0300

    Kayıt 3.25

---
+++ Assets/Scripts/PlayerHareket.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerHareket : MonoBehaviour
+{
+    Vector2 moveInput;
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D hamRigidBody;
+    void Start()
+    {
+        hamRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+     void OnMove(InputValue Value)
+    {
+        moveInput = Value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+    void Run()
+    {
+        Vector2 isoDirection = new Vector2(moveInput.x - moveInput.y, moveInput.x + moveInput.y / 2f);
+        hamRigidBody.linearVelocity = isoDirection.normalized * runSpeed;
+    }
+
+}
