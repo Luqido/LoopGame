267a55d701375ab70d4551606f31b7226cba3c47
Sinan Ermiş <hello@sinanermis.com>
2025-08-01 03:41:31 +0300

    handle around track movement, simplify existing code

--- Assets/Scripts/HamsterDoTween.cs
+++ Assets/Scripts/HamsterDoTween.cs
@@ -1,92 +1,48 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 using System.Collections;
+using DG.Tweening.Core.Easing;
 
 public class HamsterDoTween : MonoBehaviour
 {
-    bool firstRound = true;
-    private Sequence yol;
-    [SerializeField] float sure = 2;
     Animator anim;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    IEnumerator  Start()
+    [SerializeField] private TrackMoveHandler trackMoveHandler;
+
+    private void Awake()
     {
         anim = GetComponent<Animator>();
-        //SiraliHaraket();
-        while(true)
-        {
-            yield return SiraliHaraket();
-        }
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        trackMoveHandler.MoveAlongTrack(transform, OnWaypointReached);
     }
-    //void SiraliHaraket()
-    //{
-    //    if (yol != null && yol.IsActive()) return;
-    //    yol = DOTween.Sequence();
-    //    Vector3 hedef1 = new Vector3(-6.55f, 0.2f, 0);
-    //    Vector3 tersScale = new Vector3(-1, 1, 1);
-    //    Vector3 tersScale2 = new Vector3(1, 1, 1);
-    //    Vector3 hedef2 = new Vector3(0.5f, -3.5f, 0);
-    //    Vector3 hedef3 = new Vector3(7f, -0.2f, 0);
-    //    Vector3 hedef4 = new Vector3(0.22f, 3.33f, 0);
-    //    yol.AppendCallback(() => transform.localScale = tersScale2);
-    //    yol.Append(transform.DOMove(hedef1, sure).SetSpeedBased().SetEase(Ease.OutSine));
-    //    yol.Append(transform.DOScale(tersScale, 0).SetEase(Ease.Linear));
-    //    yol.Append(transform.DOMove(hedef2, sure).SetSpeedBased().SetEase(Ease.OutSine));
-    //    yol.AppendCallback (()=> anim.SetBool("Anim1", true));
-    //    yol.Append(transform.DOMove(hedef3, sure).SetSpeedBased().SetEase(Ease.OutSine));
-    //    yol.Append(transform.DOScale(tersScale2,0));
-    //    yol.Append(transform.DOMove(hedef4, sure).SetSpeedBased().SetEase(Ease.OutSine));
-    //    yol.AppendCallback(() => anim.SetBool("Anim1", false));
-
-
-
-    //    yol.SetLoops(-1);
-
-    //}
-    IEnumerator SiraliHaraket()
+    
+    private void OnWaypointReached(int index)
     {
-        Vector3 hedef1 = new Vector3(-6.55f, 0.2f, 0);
         Vector3 tersScale = new Vector3(-1, 1, 1);
         Vector3 tersScale2 = new Vector3(1, 1, 1);
-        Vector3 hedef2 = new Vector3(0.5f, -3.5f, 0);
-        Vector3 hedef3 = new Vector3(7f, -0.2f, 0);
-        Vector3 hedef4 = new Vector3(0.22f, 3.33f, 0);
-        //var closestPointOnLine = ClosestPointOnLine(transform.position, hedef4, hedef1);
-        var timeItTakes = (hedef4 - transform.position).magnitude / sure;
 
-        var firstTween = transform.DOMove(hedef1, sure).SetSpeedBased().SetEase(Ease.OutSine).Pause();
-
-        if (firstRound)
+        if (index == 0)
         {
-            firstRound = false;
-            firstTween.Goto(timeItTakes);
-
+            transform.localScale = tersScale;
         }
-        firstTween.Play();
 
-        yield return firstTween.WaitForCompletion();
-        transform.localScale = tersScale2;
-        yield return transform.DOMove(hedef1, sure).SetSpeedBased().SetEase(Ease.OutSine).WaitForCompletion();
-        transform.localScale = tersScale;
-        yield return transform.DOMove(hedef2, sure).SetSpeedBased().SetEase(Ease.OutSine).WaitForCompletion();
-        anim.SetBool("Anim1", true);
-        yield return transform.DOMove(hedef3, sure).SetSpeedBased().SetEase(Ease.OutSine).WaitForCompletion();
-        transform.localScale = tersScale2;
-        yield return transform.DOMove(hedef4, sure).SetSpeedBased().SetEase(Ease.OutSine).WaitForCompletion();
-        anim.SetBool("Anim1", true);
-    }
-    Vector3 ClosestPointOnLine(Vector3 point, Vector3 a, Vector3 b)
-    {
-        Vector3 ab = b - a;
-        Vector3 ap = point - a;
-        float t = Vector3.Dot(ap, ab) / Vector3.Dot(ab, ab);
-        return a + t * ab;
+        if (index == 1)
+        {
+            anim.SetBool("Anim1", true);
+        }
+
+        if (index == 2)
+        {
+            transform.localScale = tersScale2;
+        }
+
+        if (index == 3)
+        {
+            anim.SetBool("Anim1", false);
+            transform.localScale = tersScale2;
+        }
     }
 }
--- Assets/Scripts/TrenDoTween.cs
+++ Assets/Scripts/TrenDoTween.cs
@@ -1,65 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using DG.Tweening;
 
 public class TrenDoTween : MonoBehaviour
 {
-    private Sequence yol;
-    [SerializeField] float sure = 2f;
-    [SerializeField] AnimationCurve ad;
     Animator TrenAnim;
-    SpriteRenderer trenSprite;
-    [SerializeField] Sprite sp;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TrackMoveHandler trackMoveHandler;
+    
     void Awake()
     {
-        DOTween.SetTweensCapacity(500,500);
+        TrenAnim = GetComponent<Animator>();
     }
+
     void Start()
     {
-        TrenAnim = GetComponent<Animator>();
-        trenSprite = GetComponent<SpriteRenderer>();
-        
+
+        trackMoveHandler.MoveAlongTrack(transform, OnWaypointReached);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnWaypointReached(int index)
     {
-        SiraliHaraket();
+        switch (index)
+        {
+            case 0:
+                TrenAnim.SetTrigger("Trigger1");
+                break;
+            case 1:
+                TrenAnim.SetTrigger("Trigger2");
+                break;
+            case 2:
+                TrenAnim.SetTrigger("Trigger3");
+                break;
+            case 3:
+                TrenAnim.SetTrigger("Trigger4");
+                TrenAnim.SetTrigger("Trigger5");
+                transform.localScale = Vector3.one;
+                break;
+        }
     }
-    void SiraliHaraket()
-    {
-        
-        if (yol != null && yol.IsActive()) return; 
-
-        yol = DOTween.Sequence();
-        Vector3 hedef1 = new Vector3(-6.55f, 0.2f, 0);
-        Vector3 tersScale = new Vector3(-1, 1, 1);
-        Vector3 tersScale2 = new Vector3(1, 1, 1);
-        Vector3 hedef2 = new Vector3(0.5f, -3.5f, 0);
-        Vector3 hedef3 = new Vector3(7f, -0.2f, 0);
-        Vector3 hedef4 = new Vector3(0.22f, 3.33f, 0);
-        yol.AppendCallback(() => transform.localScale = tersScale2);
-        yol.Append(transform.DOMove(hedef1, sure).SetSpeedBased().SetEase(Ease.OutSine));
-        yol.AppendCallback(() => TrenAnim.SetTrigger("Trigger1"));
-        yol.Append(transform.DOMove(hedef2, sure).SetSpeedBased().SetEase(Ease.Linear));
-        yol.AppendCallback(() => TrenAnim.SetTrigger("Trigger2"));
-        yol.Append(transform.DOMove(hedef3, sure).SetEase(Ease.Linear));
-        yol.AppendCallback(() => TrenAnim.SetTrigger("Trigger3"));
-        yol.Append(transform.DOMove(hedef4, sure).SetEase(Ease.Linear));
-        yol.AppendCallback(() => TrenAnim.SetTrigger("Trigger4"));
-        yol.AppendCallback(() => TrenAnim.SetTrigger("Trigger5"));
-
-
-
-
-
-
-
-
-        yol.SetLoops(-1);
-
-        
-    }
-
-
 }
