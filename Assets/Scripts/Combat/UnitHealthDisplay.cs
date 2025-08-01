020d7b004f8942daa045cb5cd12437181945262c
Sinan Ermiş <hello@sinanermis.com>
2025-08-01 22:01:17 +0300

    add combat button sprites

--- Assets/Scripts/Combat/UnitHealthDisplay.cs
+++ Assets/Scripts/Combat/UnitHealthDisplay.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 public class UnitHealthDisplay : MonoBehaviour
 {
     [SerializeField] private Image healthBar;
+    [SerializeField] private SlicedFilledImage slicedFilledHealthBar;
     [SerializeField] private Unit unit;
-
+    
     private void Awake()
     {
         unit.HealthChanged += UpdateHealthBar;
@@ -14,6 +15,14 @@ public class UnitHealthDisplay : MonoBehaviour
 
     private void UpdateHealthBar(int previousHp, int currentHp, int startingHp)
     {
-        healthBar.DOFillAmount((float)currentHp / startingHp, 0.3f);
+        if (healthBar != null)
+            healthBar.DOFillAmount((float)currentHp / startingHp, 0.3f);
+
+        if (slicedFilledHealthBar != null)
+            DOTween.To(
+                () => slicedFilledHealthBar.fillAmount, 
+                (f) => slicedFilledHealthBar.fillAmount = f,
+                (float)currentHp / startingHp, 
+                0.3f);
     }
 }
